e3238a764b3398e88055d17eba94b0da860979da
Reece Harris <reece@codegalaxy.co.uk>
2023-05-03 13:15:11 +0100

    new file:   index.js 	new file:   package-lock.json 	new file:   package.json 	new file:   test.js 	new file:   views/index.ejs

---
+++ index.js
@@ -0,0 +1,54 @@
+const openpgp = require('openpgp'); // Import the openpgp library
+const crypto = require('crypto'); // Import the crypto library
+
+class pkAuth {
+  constructor(algorithm = 'ed25519') { // A constructor function that takes an algorithm parameter and sets it as a property of the class
+    this.algorithm = algorithm;
+  }
+
+  generateKeyPair = async (id = crypto.randomBytes(127).toString('hex')) => { // An async function that generates a PGP key pair using the specified algorithm and an optional ID parameter
+    try {
+      const keyPair = openpgp.generateKey({ curve: this.algorithm, userIDs: [{ uuid: id }], format: 'armored' }); // Generate the key pair using openpgp library
+      const keyPairObject = await keyPair.then(data => { return data }); // Wait for the key pair to generate and convert the data to an object
+      keyPairObject.algorithm = this.algorithm; // Set the algorithm property of the key pair object
+      return keyPairObject // Return the key pair object
+    } catch (error) {
+      throw error(error)
+    }
+  };
+
+  generatePublic = async (privateKey) => { // An async function that generates a public key from a private key
+    const readKey = openpgp.readPrivateKey({ armoredKey: privateKey }) // Read the private key using openpgp library
+    const readKeyObject = await readKey.then(data => { return data }) // Wait for the private key to read and convert the data to an object
+    return { publicKey: readKeyObject.toPublic().armor() } // Return an object containing the public key
+  };
+
+  encryptPrivateKey = async (privateKey, passphrase) => { // An async function that encrypts a private key with a passphrase
+    try {
+      const readKey = openpgp.readPrivateKey({ armoredKey: privateKey }) // Read the private key using openpgp library
+      const readKeyObject = await readKey.then(data => { return data }) // Wait for the private key to read and convert the data to an object
+      const encryptKey = openpgp.encryptKey({ privateKey: readKeyObject, passphrase: passphrase }) // Encrypt the private key using openpgp library
+      return (await encryptKey).armor() // Wait for the key to encrypt and return the encrypted key
+    } catch (error) {
+      throw error(error)
+    }
+  }
+
+  decryptPrivateKey = async (encryptedPrivateKey, passphrase) => { // An async function that decrypts an encrypted private key with a passphrase
+    try {
+      const readKey = openpgp.readPrivateKey({ armoredKey: encryptedPrivateKey }) // Read the encrypted private key using openpgp library
+      const readKeyObject = await readKey.then(data => { return data }) // Wait for the encrypted private key to read and convert the data to an object
+      const decryptKey = openpgp.decryptKey({ privateKey: readKeyObject, passphrase: passphrase }) // Decrypt the private key using openpgp library
+      return (await decryptKey).armor() // Wait for the key to decrypt and return the decrypted key
+    } catch (error) {
+      throw error(error)
+    }
+  }
+
+  validateKeys = async (privateKey, publicKey) => { // An async function that validates a public key against a private key
+    const generatePublic = (await this.generatePublic(privateKey)).publicKey // Generate the public key from the private key using the generatePublic function
+    return publicKey === generatePublic // Compare the generated public key with the provided public key and return the result
+  };
+}
+
+module.exports = pkAuth // Export the class from the module
---
+++ test.js
@@ -0,0 +1,40 @@
+const pkAuth = new (require('./index.js'))('curve25519')
+
+const sqlite3=require('sqlite3').verbose();
+const db=new sqlite3.Database(':memory:');
+const database=(sql,data=[],callback=()=>{}) => {db.all(sql, data, callback);};
+
+database(`CREATE TABLE IF NOT EXISTS creds (id INTEGER PRIMARY KEY AUTOINCREMENT,public_key TEXT,revocation_certificate TEXT);`, [], (err) => {if (err) {console.log(err)}})
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/register', async (req, res) => {
+  const keys = await pkAuth.generateKeyPair()
+  database('INSERT INTO creds (public_key,revocation_certificate) VALUES (?,?)', [keys.publicKey, keys.revocationCertificate])
+  res.json(keys)
+})
+
+app.post('/login', async (req, res) => {
+  const privateKey = req.body.privateKey;
+  const publicKey = (await pkAuth.generatePublic(privateKey)).publicKey;
+
+  database('SELECT * FROM creds WHERE public_key = ?', [publicKey], (err, rows) => {
+    res.json(rows.length === 1 ? {success: true,user: rows[0]} : {success: false,error: 'Public key does\'nt exist'})
+  })
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
